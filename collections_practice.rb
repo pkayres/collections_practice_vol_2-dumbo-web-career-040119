f33ea0e8ffb9944264182581639cb6f0a46b6887
Pete A <peter.ayresmusic@gmail.com>
2019-03-21 21:26:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,76 @@
-# your code goes here
+require'pry'
+
+def begins_with_r(words_array)
+  words_array.each do |words|
+      if words[0] == 'r'
+        else
+        return false
+      end
+  end
+  return true
+end
+
+def contain_a(words_array)
+  a_words = []
+  words_array.each do |word|
+    if word.include?('a')
+      a_words << word
+    end
+  end
+  return a_words
+end
+
+def first_wa(words_array)
+  words_array.each do |word|
+    if word[0..1].include?('wa')
+      return word
+    end
+  end
+end
+
+def remove_non_strings(random_array)
+  strings = []
+  random_array.each do |item|
+    if item.is_a?(String)
+      strings << item
+    else
+      random_array.delete(item)
+    end
+  end
+  return strings
+end
+#
+def count_elements(array)
+  result = []
+  array.each do |key_pair|
+    if name = result.detect{|x| x[:name] == key_pair[:name]}
+        name[:count] += 1
+    else
+      key_pair[:count] = 1
+      result << key_pair
+      end
+    end
+    return result
+end
+
+# We have a hash with data titeld keys and we have a hash titled data
+# We want to take our keys data and add it to our data hash
+# First we should loop through our keys hash, determine if it's new key/value pair then push it into our new hash
+# If we encounter the same key/pair we can skip that k/v pair and loop through the remainder of our hash
+# Finally we want to return our new hash that we're created.
+def merge_data(keys,data)
+  new_hash = []
+  keys.each do |key_data|
+    name_key = key_data[:first_name]
+    motto_key = key_data[:motto]
+      data.each do |data_hash|
+        data_hash.each do |data_key, data_value|
+          if data_value == data[:awesomeness]
+            data_value[awesomeness] = key_data[:first_name]
+
+    binding.pry
+          end
+        end
+      end
+    end
+end
