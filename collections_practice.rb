36479e455ac46b915b818840ca01ae297cc9eda6
Pete A <peter.ayresmusic@gmail.com>
2019-03-21 22:17:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,10 +62,13 @@ def merge_data(keys,data)
   new_array = []
   keys.each do |keys_hash|
     keys_hash.each do |key_pair|
-binding.pry
+      name_key = keys_hash[:first_name]
+      motto_key = keys_hash[:motto]
       data.each do |data_hash|
         data_hash.each do |data_name, data_hash_2|
           if data_name == name_key
+            new_name = {keys_hash.key(name_key) => name_key}
+            new_motto = {keys_hash.key(motto_key)=> motto_key}
 
                   data_hash_2 <<  new_name
 
